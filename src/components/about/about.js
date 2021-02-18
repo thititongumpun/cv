1f23d9acae18e43e8e8f497286e11e8403babfdd
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-02-18 21:09:33 +0700

    add chart

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -5,6 +5,7 @@ export default ({ title = 'about', text = '' }) => {
     <div>
       <h1>{title}</h1>
       <p dangerouslySetInnerHTML={{ __html: text }}></p>
+      <figure><embed src="https://wakatime.com/share/@thititongumpun/58292e7a-64bc-45cb-a7d6-9f222ad346d9.svg"></embed></figure>
     </div>
   )
 }
